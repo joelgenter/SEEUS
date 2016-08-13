58be72e9c1169ef9e4d6247d9c87a33a81eac8e5
joelgenter <joelgenter@outlook.com>
2016-08-13 14:48:00 -0400

    added jquery to index changed iframes in index.php to divs

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 <head>
 <meta charset="UTF-8">
 <link rel="stylesheet" type="text/css" href="css/template.css">
+<script src="jquery/jquery-3.1.0.min.js"></script>
 </head>
 <body onblur="pauseVideo()" onfocus="playVideo()">
 <div id="videoContainer"> 
@@ -14,12 +15,9 @@
 <div id="centerPane">
     <div id="leftPane">
         <img id="seeusLogo" src="media/seeus_logo_transparent.png">
-        <iframe id="iframeMenu" src="menu.php" marginwidth="0" 
-            marginheight="0" hspace="0" vspace="0" frameborder="0" scrolling="no"></iframe>
+        <div id="menuContainer"></div>
     </div>
-    <iframe id="iframeContent" src="http://localhost/seeus/iframe/escorts/archive.php" 
-        marginwidth="0" marginheight="0" hspace="0" vspace="0" frameborder="0" 
-        scrolling="no"></iframe>
+    <div id="contentContainer"></div>
     <div id="rightBorder"></div> <!--I should check to see if I can just add a border to the right side of '#centerPane'-->
 </div>
 <!---------------------------------Javascript--------------------------------->
