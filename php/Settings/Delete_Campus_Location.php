1d7673b8986ddabe47e2889416194dc6efe8e0b2
Joel Genter <joelgenter@outlook.com>
2017-03-14 15:30:44 -0400

    center tables

--- php/Settings/Delete_Campus_Location.php
+++ php/Settings/Delete_Campus_Location.php
@@ -6,16 +6,13 @@ Security::assertUserIs(['authoritarian']);
 
 $campusLocationToDelete  = Security::cleanInput($_POST['campusLocationToDelete']);
 
-$errorMessages = [];
-if (!preg_match("/[a-z0-9 ]/", $campusLocationToDelete)) {
-    
-}
-
 Utility::connectDB();
 
 $sql = 'UPDATE campus_locations
         SET Enabled = 0
         WHERE Name = "' . $campusLocationToDelete . '"';
+
+$errorMessages = [];
 
 if (!Utility::$dbConnection->query($sql)) {
     array_push($errorMessages, "Error deleting campus location");
